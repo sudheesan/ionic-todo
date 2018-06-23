c7db2e02b7e63aab1c01534faa8875d996c23531
Sudheesan <surendransudheesan@gmail.com>
2018-06-23 16:37:00 +0530

    Intial Commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,14 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('demo-todo', ['ionic','LocalStorageModule'])
 
-.run(function($ionicPlatform) {
+app.config(function(localStorageServiceProvider){
+  localStorageServiceProvider
+  .setPrefix('demo-todo');
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
